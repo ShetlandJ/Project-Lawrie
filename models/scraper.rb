a05648ecfa631dbb1efcc1b995d84d4592b152da
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-01-16 19:32:30 +0000

    content now displays on screen

--- models/scraper.rb
+++ models/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
 
     @submitted_urls = []
 
-    url_array = ["http://www.bbc.co.uk/news/business-42703549", "http://www.eveningtimes.co.uk/news/15837175.Heavy_snow_hits_Glasgow_roads_and_schools_as_Met_Office_upgrades_weather_warning_to_amber/"]
+    url_array = ["https://www.scotsman.com/business/companies/tech/food-tech-outfit-morsum-to-invest-3m-in-edinburgh-1-4438072", "http://www.bbc.co.uk/news/business-42691657"]
 
     return scrape(url_array)
 
@@ -21,33 +21,33 @@ class Scraper
       if (url.include? "bbc.co.uk")
         produce_url(url, ".story-body__h1", "(BBC News)")
       elsif (url.include? "guardian.com")
-        guardian()
+        produce_url_gsub(url, ".content__headline", "(Guardian)")
       elsif (url.include? "independent.co.uk")
-        independent()
+        produce_url(url, "h1[itemprop='headline']", "(The Independent)")
       elsif (url.include? "eveningtimes.co.uk")
-        evening_times()
+        produce_url(url, "h1[class='headline semi-loud']", "(Evening Times)")
       elsif (url.include? "times.co.uk")
-        times()
+        produce_url(url, "h1[class='Article-headline Headline Headline--article']", "(The Times)")
       elsif (url.include? "telegraph.co.uk")
-        telegraph()
+        produce_url_gsub(url, ".headline__heading", "(The Telegraph)")
       elsif (url.include? "thisismoney.co.uk")
-        this_is_money()
+        produce_url(url, "h1", "(This Is Money)")
       elsif (url.include? "sun.co.uk")
-        sun_money()
+        produce_url(url, ".article__headline", "(The Sun)")
       elsif (url.include? "mirror.co.uk")
-        mirror_money()
+        produce_url(url, "h1[itemprop='headline name']", "(The Mirror)")
       elsif (url.include? "creditstrategy.co.uk")
-        credit_strategy()
+        produce_url(url, "h1[class='aos-ArticleTitle aos-DS72-H1 aos-MBXS5']", "(Credit Strategy)")
       elsif (url.include? "credit-connect.co.uk")
-        credit_connect()
+        produce_url(url, "h1[class='entry-title entry-title--in-cover']", "(Credit Connect)")
       elsif (url.include? "reuters.com")
-        reuters()
+        produce_url(url, ".ArticleHeader_headline_2zdFM", "(Reuters)")
       elsif (url.include? "dailyrecord.co.uk")
-        daily_record()
+        produce_url(url, "h1[itemprop='headline name']", "(Daily Record)")
       elsif (url.include? "heraldscotland.com")
-        herald()
+        produce_url(url, "h1[class='headline semi-loud']", "(The Herald)")
       elsif (url.include? "scotsman.com")
-        scotsman()
+        produce_url(url, ".article-header__title", "(The Scotsman)")
       end
     end
   end
@@ -61,92 +61,17 @@ class Scraper
       "url": url
     }
     @submitted_urls.push(my_url_object)
-    p @submitted_urls
   end
 
-
-  def guardian()
+  def produce_url_gsub(url, css, news_org)
     parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css(".content__headline")
-    p headline.children.text.gsub("\n",'')
-  end
-
-  def independent()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[itemprop='headline']")
-    p headline.children.text
-  end
-
-  def times()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[class='Article-headline Headline Headline--article']")
-    p headline.children.text
-  end
-
-  def telegraph()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css(".headline__heading")
-    p headline.children.text.gsub("\n",'')
-  end
-
-  def this_is_money()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1")
-    p headline.children.text
-  end
-
-  def sun_money()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css(".article__headline")
-    p headline.children.text
-  end
-
-  def mirror_money()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[itemprop='headline name']")
-    p headline.children.text
-  end
-
-  def credit_strategy()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[class='aos-ArticleTitle aos-DS72-H1 aos-MBXS5']")
-    p headline.children.text
-  end
-
-  def credit_connect()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[class='entry-title entry-title--in-cover']")
-    p headline.children.text
-  end
-
-  def reuters()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css(".ArticleHeader_headline_2zdFM")
-    p headline.children.text
-  end
-
-  def daily_record()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[itemprop='headline name']")
-    p headline.children.text
-  end
-
-  def herald()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[class='headline semi-loud']")
-    p headline.children.text
-  end
-
-  def scotsman()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css(".article-header__title")
-    p headline.children.text
-  end
-
-  def evening_times()
-    parse_page ||= Nokogiri::HTML(@doc)
-    headline = parse_page.css("h1[class='headline semi-loud']")
-    p headline.children.text
+    headline = parse_page.css(css)
+    my_url_object = {
+      "content": headline.children.text.gsub("\n", ''),
+      "news_org": news_org,
+      "url": url
+    }
+    @submitted_urls.push(my_url_object)
   end
 
   def return_urls()
